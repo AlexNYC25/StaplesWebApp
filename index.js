5d304ea7a13bc3c74944e97fd581a5c8c6306cb3
Alexis Montes <montes.alexis25@gmail.com>
2020-05-25 20:44:30 -0400

    started: set up initial routing set up

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 //use express.js for web
 const express = require('express');
-// handlbars engine handling
-const hbs = require('express-handlebars');
+const mongo_lib = require('./mongoLib.js')
 
 let app = express();
 
 
-app.use(express.static('/public'))
-
-// sets handlebars engine as view engine
-app.engine('handlebars', hbs());
-app.set('view engine', 'handlebars');
-
-
+/*
+    TODO:
+    Main api get request for the home page
+    Should return all products from mongodb database when called by the front end
+*/
 app.get('/', (req, res) => {
     // mockup will be index
-    res.render('home');
+    res.send('Hello World');
 });
 
-app.get('/item', (req, res) =>{
-    // add in render functionality for product items
-    // TODO: mockup will be 'product'
+/*
+    TODO:
+    get request where a string is passed from the front end to the back end
+    and pass that string into external library module and generate data from it
+*/
+app.get('/:str', (req, res) => {
+    console.log(req.params.str);
+    res.send('searching for products using keyword ' + req.params.str);
+})
+
+/*
+    TODO:
+    get request for item using passed id and return data on that product
+*/
+app.get('/item/:id', (req, res) =>{
+    // TODO: set up backend response to return data for item number
+    console.log(req.params.id);
+    res.send('searching for product with id ' + req.params.id);
 })
 
 

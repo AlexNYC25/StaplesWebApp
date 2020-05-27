390bf22fa2f2f4c1c97bb8595017534d79919545
Alexis Montes <montes.alexis25@gmail.com>
2020-05-27 10:57:02 -0400

    created navbar with basic buttons and layout

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+class Navbar extends React.Component{
+    render() {
+        return (
+            <div id="main-navbar" class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3  border-bottom shadow-sm">
+            <h5 class="my-0 mr-md-auto font-weight-normal">Staples Web App</h5>
+            <div>
+            
+            </div>
+            <nav class="my-2 my-md-0 mr-md-3">
+                <a class="p-2 text-dark" href="#">All Items</a>
+                <a class="p-2 text-dark" href="#">Displays</a>
+                <a class="p-2 text-dark" href="#">Github</a>
+            </nav>
+            <a class="btn main-button" href="#">Sign In</a>
+            </div>
+        );
+    }
+
+}
+
+export default Navbar;

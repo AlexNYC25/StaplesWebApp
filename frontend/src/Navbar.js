ccff3651a8cc4ca11992b8a7884082bcd4f75626
Alexis Montes <montes.alexis25@gmail.com>
2020-06-01 15:46:39 -0400

    added basic navbar elements

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -5,16 +5,16 @@ class Navbar extends React.Component{
     render() {
         return (
             <div id="main-navbar" class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3  border-bottom shadow-sm">
-            <h5 class="my-0 mr-md-auto font-weight-normal">Staples Web App</h5>
-            <div>
+                <h5 class="my-0 mr-md-auto font-weight-normal">Staples Web App</h5>
+                <div>
             
-            </div>
-            <nav class="my-2 my-md-0 mr-md-3">
-                <a class="p-2 text-dark" href="#">All Items</a>
-                <a class="p-2 text-dark" href="#">Displays</a>
-                <a class="p-2 text-dark" href="#">Github</a>
-            </nav>
-            <a class="btn main-button" href="#">Sign In</a>
+                </div>
+                <nav class="my-2 my-md-0 mr-md-3 navbar-text">
+                    <a class="p-2 " href="#">All Items</a>
+                    <a class="p-2 " href="#">Displays</a>
+                    <a class="p-2 " href="#">Github</a>
+                </nav>
+                <a class="btn main-button navbar-text" href="#">Sign In</a>
             </div>
         );
     }

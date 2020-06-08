e8e5ffbccfa5b079a49ceee009d7b9e5a5df40df
Alexis Montes <montes.alexis25@gmail.com>
2020-06-07 23:54:41 -0400

    added link for data manipulation page

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -10,9 +10,10 @@ class Navbar extends React.Component{
             
                 </div>
                 <nav class="my-2 my-md-0 mr-md-3 navbar-text">
-                    <a class="p-2 " href="#">All Items</a>
+                    <a class="p-2 " href="/">All Items</a>
                     <a class="p-2 " href="#">Displays</a>
                     <a class="p-2 " href="#">Github</a>
+                    <a className="p-2" href="/DataManipulation">Data Control Panel</a>
                 </nav>
                 <a class="btn main-button navbar-text" href="#">Sign In</a>
             </div>

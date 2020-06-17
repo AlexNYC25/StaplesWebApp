d1792fd0d437b84f22c1f8ddb21a6e2a1e1ca95c
Alexis Montes <montes.alexis25@gmail.com>
2020-06-16 20:53:48 -0400

    removed test json from initial testing

--- frontend/src/ProductLayout.js
+++ frontend/src/ProductLayout.js
@@ -1,58 +1,6 @@
 
 import React from 'react';
 
-// temp json data for proof of concept
-const originalInfo = [
-	{
-	  "_id": "5ecdaa5fe7c0c2f7c182d62b",
-	  "index": 0,
-	  "age": 30,
-	  "eyeColor": "blue",
-	  "name": "Josephine Haynes"
-	},
-	{
-	  "_id": "5ecdaa5fcea20e0a2d2f0d35",
-	  "index": 1,
-	  "age": 23,
-	  "eyeColor": "blue",
-	  "name": "Georgette Perry"
-	},
-	{
-	  "_id": "5ecdaa5f7e39e9d9fb5d9aac",
-	  "index": 2,
-	  "age": 32,
-	  "eyeColor": "brown",
-	  "name": "Katie Larson"
-	},
-	{
-	  "_id": "5ecdaa5faa289c8693dd85c8",
-	  "index": 3,
-	  "age": 20,
-	  "eyeColor": "green",
-	  "name": "Horton Montoya"
-	},
-	{
-	  "_id": "5ecdaa5f68ce374247a6ee5d",
-	  "index": 4,
-	  "age": 31,
-	  "eyeColor": "green",
-	  "name": "Carolina Little"
-	},
-	{
-	  "_id": "5ecdaa5f068921e94e5e7743",
-	  "index": 5,
-	  "age": 35,
-	  "eyeColor": "green",
-	  "name": "Wise Hopkins"
-	},
-	{
-	  "_id": "5ecdaa5fa981b8ac9aa96b1f",
-	  "index": 6,
-	  "age": 20,
-	  "eyeColor": "blue",
-	  "name": "Sara Fitzpatrick"
-	}
-  ];
 
 
 class ProductLayout extends React.Component{
@@ -77,30 +25,15 @@ class ProductLayout extends React.Component{
   	}
   
   
-
-	/*
-		Pass user input to backend api and get json with products that 
-		are found from search results
-
-		Currently when any user input is passed then the 
-		state's info is set to an empty array
-	*/
 	handleSubmit(event) {
-		//alert('A name was submitted: ' + this.state.value);
 		this.componentDidMount();
 		
   	}
   
-	/*
-		Fetches backend response at certain path and using return data 
-		set the state info value as the new data
 
-		TODO:
-	  		Test if component will still render content using the same get path '/id'
-	*/
  	async componentDidMount(){
 		if(this.state.value == ''){
-			let link = "http://localhost:8080/";
+			let link = "http://localhost:8080/allProducts";
 			fetch(link)
 				.then(response => response.json())
 				.then(data => this.setState({info: data}))

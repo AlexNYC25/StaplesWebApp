319ae019bb6915197aa810e5e505831f26ecec72
Alexis Montes <montes.alexis25@gmail.com>
2020-06-21 23:39:37 -0400

    moved data manipulation component to its own folder

--- frontend/src/components/DataManipulation/DataManipulation.js
+++ frontend/src/components/DataManipulation/DataManipulation.js
@@ -1,32 +1,18 @@
 import React from 'react'
-
-import Navbar from './Navbar'
 import { Button } from '@material-ui/core'
-// Place buttons for different data manipulation functions
 
-// One for inserting a new Product with a product id and Name
-
-// one for changing a product Name using the product id
-
-// one for adding images to a product using id
-
-// one for adding location names to a product id
-
-// one for changing product price 
 
 class DataManipulation extends React.Component {
     constructor(props){
         super(props)
-
     }
 
     render() {
         return (
             <div className="data-options">
-                <Navbar />
                 <div className="data-btn-section container">
                     <div className="row btn-row">
-                        <Button href="/NewProduct" className="data-btn col col-4 ml-autp">
+                        <Button href="/NewProduct" className="data-btn col col-4 mr-auto">
                             Add New Products
                         </Button>
                         <Button href="/NewProductName" className="data-btn col col-3 ml-auto">
@@ -45,8 +31,6 @@ class DataManipulation extends React.Component {
                         </Button>
                     </div>
                 </div>
-                
-  
             </div>
         )
     }

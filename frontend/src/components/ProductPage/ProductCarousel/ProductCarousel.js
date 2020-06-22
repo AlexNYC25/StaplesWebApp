baadcfc81e7b54186c6c07c08aa0ac97f4c552c1
Alexis Montes <montes.alexis25@gmail.com>
2020-06-22 14:59:13 -0400

    moved productCarousel to it's own folder and added alt tags to images

--- frontend/src/components/ProductPage/ProductCarousel/ProductCarousel.js
+++ frontend/src/components/ProductPage/ProductCarousel/ProductCarousel.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import Carousel from '@brainhubeu/react-carousel'
 import '@brainhubeu/react-carousel/lib/style.css'
-import Dots from 'material-ui-dots'
 
-
-import './App.css'
-
-/*
-    Need to center material dots
-*/
+//import './App.css'
 
 class ProductCarousel extends React.Component{
     constructor(props) {
@@ -51,23 +45,16 @@ class ProductCarousel extends React.Component{
                     arrows
                     infinite
                 >
-                    
-                
                     {
                         this.props.Images
-                            ?this.props.Images.map(img => <img width="300px" height="150px" src={img}></img>)
-                            : <img  src='https://media.giphy.com/media/hEc4k5pN17GZq/giphy.gif' />
+                            ?this.props.Images.map(img => <img width="300px" height="150px" alt="product image" src={img}></img>)
+                            : <img  src='https://media.giphy.com/media/hEc4k5pN17GZq/giphy.gif' alt="default image"/>
                     }
-                
-        
                 </Carousel>
                 
             </div>
         )
     }
 }
-
-//ReactDOM.render(<ProductCarousel />, document.querySelector('.carousel'))
-//ReactDOM.render(<Carousel />, document.querySelector('.demo-carousel'));
 
 export default ProductCarousel;
